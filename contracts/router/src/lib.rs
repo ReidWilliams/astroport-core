61f10463aa49eb7a9f7428a0119c8a3350d4e66f
Reid Williams <reid@computable.io>
2022-01-22 13:21:57 -0800

    remove contracts except token

--- contracts/router/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod contract;
-pub mod state;
-
-mod error;
-
-mod operations;
-
-#[cfg(test)]
-mod testing;
--- contracts/whitelist/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod contract;
-pub mod error;
-#[cfg(test)]
-mod integration_tests;
-pub mod state;
-
-pub use crate::error::ContractError;
